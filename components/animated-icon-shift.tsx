4496c2da90093f435781e307671cc760e186f43a
Joma Dela Cruz <jomadlcrz@gmail.com>
2026-03-19 22:15:07 +0800

    refactor: simplify AnimatedIconShift component more smoother

--- components/animated-icon-shift.tsx
+++ components/animated-icon-shift.tsx
@@ -1,36 +1,33 @@
 import { type ReactNode, useEffect, useRef } from "react";
-import {
-  Animated,
-  Easing,
-  type StyleProp,
-  type ViewStyle,
-} from "react-native";
+import { Animated, type StyleProp, type ViewStyle } from "react-native";
 
 type AnimatedIconShiftProps = {
   active: boolean;
   children: ReactNode;
   distance?: number;
-  duration?: number;
   style?: StyleProp<ViewStyle>;
 };
 
 export function AnimatedIconShift({
   active,
   children,
-  distance = 4,
-  duration = 180,
+  distance = 5,
   style,
 }: AnimatedIconShiftProps) {
   const translateX = useRef(new Animated.Value(active ? distance : 0)).current;
 
   useEffect(() => {
-    Animated.timing(translateX, {
+    Animated.spring(translateX, {
       toValue: active ? distance : 0,
-      duration,
-      easing: Easing.out(Easing.cubic),
+      damping: 16,
+      stiffness: 180,
+      mass: 0.9,
+      overshootClamping: false,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
       useNativeDriver: true,
     }).start();
-  }, [active, distance, duration, translateX]);
+  }, [active, distance, translateX]);
 
   return (
     <Animated.View style={[style, { transform: [{ translateX }] }]}>
